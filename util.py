f4ff056870daac1ed52546e4c87f195218dad276
Inferus <seva2409@gmail.com>
2024-05-23 13:46:30 +0200

    fix null division error

--- util.py
+++ util.py
@@ -1,3 +1,5 @@
 def calculate_pump_percent(ticker_before, ticker_now):
+    if ticker_before["openInterest"] == 0:
+        ticker_before["openInterest"] = 0.00000001
     return (float(ticker_now["openInterest"]) / float(ticker_before["openInterest"]) * 100) - 100
 
